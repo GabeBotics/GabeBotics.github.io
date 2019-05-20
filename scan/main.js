8a5f2a81fadabeedac4b1444bd7ee3e1a0a06139
sneaky-andy <50839724+sneaky-andy@users.noreply.github.com>
2019-05-20 12:39:43 -0400

    Removed parenthesis

--- scan/main.js
+++ scan/main.js
@@ -60,7 +60,7 @@ function Scan(){
 	output = output.replace(‘/% /‘, ‘/ /‘);
 	output = output.replace(‘*% /‘, ‘* /‘);
 	output = output.replace(‘/% *‘, ‘/ *‘);
-	output = output.replace(‘*% *), ‘* *‘);
+	output = output.replace(‘*% *', ‘* *‘);
 	output = output.replace(‘%’, ‘-‘);
 	
 	
